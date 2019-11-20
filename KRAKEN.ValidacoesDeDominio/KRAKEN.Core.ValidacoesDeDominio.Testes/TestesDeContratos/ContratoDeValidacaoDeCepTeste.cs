a800fc5e2bb42218ccdaf5b8406d6558981b8245
Thiago Chagas <chagas.bass@gmail.com>
2019-11-20 16:56:08 -0300

    efetuando validacoes de cep

---
+++ KRAKEN.ValidacoesDeDominio/KRAKEN.Core.ValidacoesDeDominio.Testes/TestesDeContratos/ContratoDeValidacaoDeCepTeste.cs
@@ -0,0 +1,47 @@
+﻿using KRAKEN.Core.ValidacoesDeDominio.Validacoes;
+using Xunit;
+
+namespace KRAKEN.Core.ValidacoesDeDominio.Testes.TestesDeContratos
+{
+    public class ContratoDeValidacaoDeCepTeste
+    {
+        [TraitAttribute("Categoria", "Testes de Contrato de ceps")]
+        [Fact(DisplayName = "DeveRetornarNotificacaoQuandoCepForVazio")]
+        public void DeveRetornarNotificacaoQuandoCepForVazio()
+        {
+            var cep = string.Empty;
+
+            var contrato = new Contrato()
+              .Requer()
+              .VerificarSeCepVazio(cep,"cep","cep vazio");
+
+            Assert.Equal(1, contrato.Notificacoes.Count);
+        }
+
+        [TraitAttribute("Categoria", "Testes de Contrato de ceps")]
+        [Fact(DisplayName = "NaoDeveRetornarNotificacaoQuandoCepNaoForVazio")]
+        public void NaoDeveRetornarNotificacaoQuandoCepNaoForVazio()
+        {
+            var cep = "24130-110";
+
+            var contrato = new Contrato()
+              .Requer()
+              .VerificarSeCepVazio(cep, "cep", "cep vazio");
+
+            Assert.Equal(0, contrato.Notificacoes.Count);
+        }
+
+        [TraitAttribute("Categoria", "Testes de Contrato de ceps")]
+        [Fact(DisplayName = "DeveRetornarVerdadeiroQNaoDeveRetornarNotificacaoQuandoCepForValidoEForFormatadouandoValorForFalso")]
+        public void NaoDeveRetornarNotificacaoQuandoCepForValidoEForFormatado()
+        {
+            var cep = "24130-110";
+
+            var contrato = new Contrato()
+              .Requer()
+              .VerificarSeCepVazio(cep, "cep", "cep vazio");
+
+            Assert.Equal(0, contrato.Notificacoes.Count);
+        }
+    }
+}
